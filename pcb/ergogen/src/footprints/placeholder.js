63654472a739bf82ec2503fe0d7828024a83f149
Stefan Marx <stefan@marx-merten.de>
2024-03-04 23:40:22 +0100

    Added Dongle PCB and 3d"

--- pcb/ergogen/src/footprints/placeholder.js
+++ pcb/ergogen/src/footprints/placeholder.js
@@ -1,37 +1,30 @@
 module.exports = {
     params: {
         designator: 'PAD',
-        width: 1,
-        height: 1,
-        front: true,
-        back: true,
-        text: '',
-        align: 'left',
-        mirrored: {type: 'boolean', value: '{{mirrored}}'},
-        net: undefined
+        label: "Placeholder",
     },
     body: p => {
 
-        const layout = (toggle, side) => {
-            if (!toggle) return ''
-            let x = 0, y = 0
-            const mirror = side == 'B' ? '(justify mirror)' : ''
-            const plus = (p.text.length + 1) * 0.5
-            let align = p.align
-            if (p.mirrored === true) {
-                if (align == 'left') align = 'right'
-                else if (align == 'right') align = 'left'
-            }
-            if (align == 'left') x -= p.width / 2 + plus
-            if (align == 'right') x += p.width / 2 + plus
-            if (align == 'up') y += p.height / 2 + plus
-            if (align == 'down') y -= p.height / 2 + plus
-            let text = ''
-            if (p.text.length) {
-                text = `(fp_text user ${p.text} (at ${x} ${y} ${p.r}) (layer ${side}.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15)) ${mirror}))`
-            }
-            return `(pad 1 smd rect (at 0 0 ${p.r}) (size ${p.width} ${p.height}) (layers ${side}.Cu ${side}.Paste ${side}.Mask) ${p.net})\n${text}`
-        }
+        // const layout = (toggle, side) => {
+        //     if (!toggle) return ''
+        //     let x = 0, y = 0
+        //     const mirror = side == 'B' ? '(justify mirror)' : ''
+        //     const plus = (p.text.length + 1) * 0.5
+        //     let align = p.align
+        //     if (p.mirrored === true) {
+        //         if (align == 'left') align = 'right'
+        //         else if (align == 'right') align = 'left'
+        //     }
+        //     if (align == 'left') x -= p.width / 2 + plus
+        //     if (align == 'right') x += p.width / 2 + plus
+        //     if (align == 'up') y += p.height / 2 + plus
+        //     if (align == 'down') y -= p.height / 2 + plus
+        //     let text = ''
+        //     if (p.text.length) {
+        //         text = `(fp_text user ${p.text} (at ${x} ${y} ${p.r}) (layer ${side}.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15)) ${mirror}))`
+        //     }
+        //     return `(pad 1 smd rect (at 0 0 ${p.r}) (size ${p.width} ${p.height}) (layers ${side}.Cu ${side}.Paste ${side}.Mask) ${p.net})\n${text}`
+        // }
 
         return `
 
@@ -43,7 +36,7 @@ module.exports = {
             (fp_text reference "${p.ref}" (at 0 -1) (layer F.SilkS)  (effects (font (size 1.27 1.27) (thickness 0.15))))
             (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
 
-            (fp_text value "Placeholder" (at 0 1 unlocked) (layer "F.Fab")
+            (fp_text value "${p.label}" (at 0 1 unlocked) (layer "F.Fab")
             (effects (font (size 1 1) (thickness 0.15)))
           (tstamp 0e4b21ae-dc92-44c4-95ab-68a452e39cec)
         )
